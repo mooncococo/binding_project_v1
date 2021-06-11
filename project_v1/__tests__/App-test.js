aa1a22b3a8142e4a0ceafa8c1d69281c6b5c21e4
moon <project0515@naver.com>
2021-06-11 15:01:43 +0900

    test커밋

--- project_v1/__tests__/App-test.js
+++ project_v1/__tests__/App-test.js
@@ -8,7 +8,7 @@ import App from '../App';
 
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
-//test
+//test test
 it('renders correctly', () => {
   renderer.create(<App />);
 });
