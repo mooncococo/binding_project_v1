900cf724cb9584c9a3162e4422dfe3f9c0706733
moon <project0515@naver.com>
2021-06-11 15:08:02 +0900

    ssg test branch

--- project_v1/__tests__/App-test.js
+++ project_v1/__tests__/App-test.js
@@ -8,7 +8,7 @@ import App from '../App';
 
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
-//test kmk
+//test ssg
 
 it('renders correctly', () => {
   renderer.create(<App />);
